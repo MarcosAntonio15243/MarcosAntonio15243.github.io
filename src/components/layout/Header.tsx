50bbe71bc15be3edcd8881b114f80129a7525b3b
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-29 01:02:11 -0300

    feat: Adicionando botão de display da navbar no Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,20 @@
+import HeaderButton from "../buttons/HeaderButton";
 import NavBar from "./NavBar";
 
 export default function Header() {
   return (
     <header className="py-3 border-b-[1px] border-[var(--color-light-gray)]">
       <div className="flex flex-row justify-between items-center max-w-[90vw] mx-auto">
-        <img src="/assets/logo.svg" alt="teste de imagem" className="cursor-pointer" />
-        <NavBar />
+        <img
+          src="/assets/logo.svg"
+          alt="teste de imagem"
+          className="cursor-pointer"
+        />
+        <NavBar className="hidden flex-row sm:flex gap-10" />
+        <div className="sm:hidden relative">
+          <HeaderButton />
+        </div>
       </div>
     </header>
-  )
+  );
 }
