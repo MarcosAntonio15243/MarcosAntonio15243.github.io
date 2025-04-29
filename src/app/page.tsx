d647ba0ce6537371139dfa7f12f4433237421224
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-29 15:52:48 -0300

    feat: Adicionando elementos da seção de apresentação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,15 @@ export default function Home() {
   return (
     <div className="">
       <Header />
-      <h1 className="">asasas</h1>
-      <IoMdMenu className="text-3xl" />
+      <main className="flex flex-col justify-center items-center">
+        <div className="mx-6 max-w-3xl">
+          <img src="/assets/profile.webp" alt="Profile Image" className="h-20 w-20 rounded-full"/>
+          <h1 className="flex flex-col">Marcos Antonio <span>Full-Stack Developer</span></h1>
+          <p>
+            I am a full stack developer dedicated to building complete solutions, combining the development of modern, functional interfaces with the implementation of robust back-end architectures. I am committed to technical excellence, continuous improvement, and delivering projects that drive value and innovation for clients and partners.
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
