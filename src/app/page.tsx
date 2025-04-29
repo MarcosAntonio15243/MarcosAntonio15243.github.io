156d7a7feb4c3a790a0240ca79ddb0fbdf991e69
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-29 15:58:46 -0300

    style: Adicionando estilizações de título h1 e parágrafos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/layout/Header";
-import { IoMdMenu } from "react-icons/io";
 
 export default function Home() {
   return (
@@ -8,7 +7,7 @@ export default function Home() {
       <main className="flex flex-col justify-center items-center">
         <div className="mx-6 max-w-3xl">
           <img src="/assets/profile.webp" alt="Profile Image" className="h-20 w-20 rounded-full"/>
-          <h1 className="flex flex-col">Marcos Antonio <span>Full-Stack Developer</span></h1>
+          <h1 className="flex flex-col">Marcos Antonio <span className="text-[var(--color-dark-gray)]">Full-Stack Developer</span></h1>
           <p>
             I am a full stack developer dedicated to building complete solutions, combining the development of modern, functional interfaces with the implementation of robust back-end architectures. I am committed to technical excellence, continuous improvement, and delivering projects that drive value and innovation for clients and partners.
           </p>
