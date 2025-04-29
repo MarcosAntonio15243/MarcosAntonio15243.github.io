0bc89422b0bb4e6c5edf788af8adbb87f2b33f99
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-29 16:36:07 -0300

    fix: Diminuindo textos em tamanhos mobile para dar espaçamento entre eles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
               I am a full stack developer dedicated to building complete solutions, combining the development of modern, functional interfaces with the implementation of robust back-end architectures. I am committed to technical excellence, continuous improvement, and delivering projects that drive value and innovation for clients and partners.
             </p>
           </section>
-          <div className="flex flex-row justify-between items-center text-[var(--color-dark-gray)] border-y-[1px] py-3 px-2 border-[var(--color-light-gray)]">
+          <div className="flex flex-row justify-between items-center max-sm:text-sm text-[var(--color-dark-gray)] border-y-[1px] py-3 px-2 border-[var(--color-light-gray)]">
             <div className="flex flex-row items-center gap-1">
-              <IoLocationOutline className="text-xl" /> Campina Grande, Paraíba, Brasil
+              <IoLocationOutline className="sm:text-xl" /> Campina Grande, Paraíba, Brasil
             </div>
             <a href="https://drive.google.com/file/d/1bdcbdjJdf5UdUBGVc3DPXeUWwqCL0DS-/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="font-semibold hover:underline">CV</a>
           </div>
