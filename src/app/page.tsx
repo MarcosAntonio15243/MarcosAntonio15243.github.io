37c2b66f19a72d4088ac6ad37553cf6339a914ef
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-29 00:50:20 -0300

    config: Adicionando React Icons as dependencias do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/layout/Header";
+import { IoMdMenu } from "react-icons/io";
 
 export default function Home() {
   return (
     <div className="">
       <Header />
       <h1 className="">asasas</h1>
+      <IoMdMenu className="text-3xl" />
     </div>
   );
 }
