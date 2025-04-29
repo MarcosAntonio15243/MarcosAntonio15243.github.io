32ba351aae38c5fe0bdf905652f5a0a98360e454
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-28 23:51:31 -0300

    feat: Adicionando elementos básicos para o header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
+import Header from "@/components/layout/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      <img src="/assets/logo.svg" alt="teste de imagem" />
-      <p className="font-roboto font-light">About</p>
-      <p className="font-roboto font-medium">About</p>
-      <p className="font-roboto font-semibold">About</p>
-      <h1>asasas</h1>
-      <h2>asasas</h2>
-      <h3>asasas</h3>
-      <h4>asasas</h4>
-      <h5>asasas</h5>
-      <h6>asasas</h6>
+      <Header />
     </div>
   );
 }
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Header() {
+  return (
+    <header>
+      <img src="/assets/logo.svg" alt="teste de imagem" />
+      <nav>
+        <a href="#">About</a>
+        <a href="#">Experience</a>
+        <a href="#">Project</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+  )
+}
