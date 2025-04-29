f45bd0172d025b41870ed492c1df8c1a493cc819
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-29 20:57:42 -0300

    style: Estilização do card de experiência com responsividade

--- src/components/cards/ExperienceCard.tsx
+++ src/components/cards/ExperienceCard.tsx
@@ -10,20 +10,33 @@ interface ExperienceCardProps extends HTMLAttributes<HTMLElement> {
   description: string;
 }
 
-export default function ExperienceCard({ srcImg, altImg="Enterprise Logo", jobTitle, enterprise, dateStart, dateEnd, description, ...props }: ExperienceCardProps) {
+export default function ExperienceCard({
+  srcImg,
+  altImg = "Enterprise Logo",
+  jobTitle,
+  enterprise,
+  dateStart,
+  dateEnd,
+  description,
+  ...props
+}: ExperienceCardProps) {
   return (
-    <div {...props}>
-      <img src={srcImg} alt={altImg} className="h-10 w-10 rounded-full"/>
-      <div>
-        <div>
-          <div>
-            <span>{jobTitle}</span>
-            <span>{enterprise}</span>
+    <div className="flex flex-col gap-2" {...props}>
+      <div className="w-full flex flex-col md:flex-row justify-between">
+        <div className="flex flex-row gap-2">
+          <img src={srcImg} alt={altImg} className="h-11 w-11 rounded-full" />
+          <div className="font-roboto flex flex-col gap-0 text-[1.1rem]">
+            <span className="font-semibold">{jobTitle}</span>
+            <span className="font-thin max-sm:text-sm max-sm:tracking-[0.03rem]">
+              {enterprise}
+            </span>
           </div>
-          <p>{dateStart} - {dateEnd || "Present"}</p>
         </div>
-        <p>{description}</p>
+        <span className="font-source-serif-4 font-light max-md:ml-13 text-nowrap text-base text-[var(--color-dark-gray)]">
+          {dateStart} - {dateEnd || "Present"}
+        </span>
       </div>
+      <p className="sm:ml-13">{description}</p>
     </div>
-  )
+  );
 }
