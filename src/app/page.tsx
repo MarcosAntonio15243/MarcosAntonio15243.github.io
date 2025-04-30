7c660a3f9b65b61316598a3236c825fdcd195294
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-30 19:33:15 -0300

    fix: Trocando link do currículo para o mais atualizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
               Paraiba, Brasil
             </div>
             <a
-              href="https://drive.google.com/file/d/1bdcbdjJdf5UdUBGVc3DPXeUWwqCL0DS-/view?usp=sharing"
+              href="https://drive.google.com/file/d/1VUnR2EF_ygJ_cYsx4itouwxfU6K1pI7S/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="font-semibold hover:underline uppercase"
