bddc0568aedd02ba4b7632d0db1534f1151a684f
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-29 22:04:08 -0300

    fix: Fazendo estilização e ajustando responsividade do card das features

--- src/components/cards/FeatureCard.tsx
+++ src/components/cards/FeatureCard.tsx
@@ -9,6 +9,7 @@ interface FeatureCardProps extends HTMLAttributes<HTMLElement> {
   description: string;
   tecnologies: string[];
   repositoryLink: string;
+  reverse?: boolean;
 }
 
 export default function FeatureCard({
@@ -18,21 +19,36 @@ export default function FeatureCard({
   description,
   tecnologies,
   repositoryLink,
+  reverse=false,
   ...props
 }: FeatureCardProps) {
   return (
-    <div {...props}>
-      <img src={srcImg} alt={altImg} className="w-1/2 h-72 rounded-full object-cover object-top" />
-      <div>
+    <div className={`flex flex-col ${reverse ? "md:flex-row-reverse" : "md:flex-row"} md:flex-row md:items-center gap-6`} {...props}>
+      <img
+        src={srcImg}
+        alt={altImg}
+        className="max-w-[22rem] h-[16rem] object-cover object-top mx-auto rounded-xl shadow-lg"
+      />
+      <div className={`flex flex-col text-center ${reverse ? "md:text-right" : "md:text-left"} gap-2`}>
         <h3>{featureTitle}</h3>
         <p>{description}</p>
         <hr className="text-[var(--color-light-gray)]" />
-        {tecnologies.map((e, index) => {
-          return (
-            <span key={index}>{e}</span>
-          )
-        })}
-        <a href={repositoryLink}><FiGithub /><LuSquareArrowOutUpRight /></a>
+        <div className={`flex flex-col gap-2 justify-center items-center ${reverse ? "md:items-end" : "md:items-start"}`}>
+          <div className="font-roboto font-thin flex flex-row w-fit gap-5">
+            {tecnologies.map((e, index) => {
+              return <span key={index}>{e}</span>;
+            })}
+          </div>
+          <a
+            href={repositoryLink}
+            className="max-md:mx-auto flex flex-row gap-1 text-xl hover:text-[var(--color-dark-gray)]"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FiGithub />
+            <LuSquareArrowOutUpRight />
+          </a>
+        </div>
       </div>
     </div>
   );
