dbf1130fc2b5dec9b461800e6f9c321b18188038
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-11-11 05:12:22 -0300

    refactor: format all files using biome

--- src/components/buttons/HeaderButton.tsx
+++ src/components/buttons/HeaderButton.tsx
@@ -5,24 +5,24 @@ import { IoMdMenu } from "react-icons/io";
 import NavBar from "../layout/NavBar";
 
 export default function HeaderButton() {
-  const [display, setDisplay] = useState("hidden");
+	const [display, setDisplay] = useState("hidden");
 
-  return (
-    <>
-      <IoMdMenu
-        className="text-4xl cursor-pointer p-1 hover:text-[var(--color-dark-gray)]"
-        onClick={() =>
-          setDisplay((d) => (d.includes("hidden") ? "flex" : "hidden"))
-        }
-      />
-      <div
-        className={`absolute ${display} flex-col z-10 right-3 bg-[var(--color-white)] border-[1px] border-[var(--color-light-gray)] p-5 gap-5 rounded-2xl rounded-tr-none`}
-      >
-        <NavBar
-          onClick={() => setDisplay("hidden")}
-          className="flex-col gap-5 items-center"
-        />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<IoMdMenu
+				className="text-4xl cursor-pointer p-1 hover:text-[var(--color-dark-gray)]"
+				onClick={() =>
+					setDisplay((d) => (d.includes("hidden") ? "flex" : "hidden"))
+				}
+			/>
+			<div
+				className={`absolute ${display} flex-col z-10 right-3 bg-[var(--color-white)] border-[1px] border-[var(--color-light-gray)] p-5 gap-5 rounded-2xl rounded-tr-none`}
+			>
+				<NavBar
+					onClick={() => setDisplay("hidden")}
+					className="flex-col gap-5 items-center"
+				/>
+			</div>
+		</>
+	);
 }
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,42 +1,42 @@
 import { HTMLAttributes } from "react";
 
 interface NavBarProps extends HTMLAttributes<HTMLElement> {
-  className?: string;
+	className?: string;
 }
 
 export default function NavBar({ className = "", ...props }: NavBarProps) {
-  const navItems = [
-    {
-      value: "About",
-      link: "#about",
-    },
-    {
-      value: "Experience",
-      link: "#experience",
-    },
-    {
-      value: "Project",
-      link: "#project",
-    },
-    {
-      value: "Contact",
-      link: "#contact",
-    },
-  ];
+	const navItems = [
+		{
+			value: "About",
+			link: "#about",
+		},
+		{
+			value: "Experience",
+			link: "#experience",
+		},
+		{
+			value: "Project",
+			link: "#project",
+		},
+		{
+			value: "Contact",
+			link: "#contact",
+		},
+	];
 
-  return (
-    <nav className={`font-roboto font-light flex ${className}`} {...props}>
-      {navItems.map((e, index) => {
-        return (
-          <a
-            key={index}
-            href={e.link}
-            className="hover:text-[var(--color-dark-gray)] hover:underline outline-none"
-          >
-            {e.value}
-          </a>
-        );
-      })}
-    </nav>
-  );
+	return (
+		<nav className={`font-roboto font-light flex ${className}`} {...props}>
+			{navItems.map((e, index) => {
+				return (
+					<a
+						key={index}
+						href={e.link}
+						className="hover:text-[var(--color-dark-gray)] hover:underline outline-none"
+					>
+						{e.value}
+					</a>
+				);
+			})}
+		</nav>
+	);
 }
